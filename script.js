35f6d88fb31eae9a33ac7ffaf403ca59bb6e17d9
rimi <a.fatimaarimi@gmail.com>
2023-07-14 17:01:11 -0400

    chef done

--- script.js
+++ script.js
@@ -15,3 +15,77 @@ console.log(chucky.law);
 console.log(chucky.votes);
 
 // Part 2
+class Person {
+  constructor(name, job, mood) {
+    this.name = name;
+    this.job = job;
+    this.mood = mood;
+  }
+  greet() {
+    console.log(`My name is ${this.name}. It's nice to meet you.`);
+  }
+  work() {
+    console.log(`I work as a ${this.job}`);
+  }
+  happy() {
+    if (mood === true) {
+      console.log(`I love being a ${this.job}`);
+    } else {
+      console.log("I want to go home");
+    }
+  }
+}
+
+
+class PostalWorker extends Person {
+  constructor(name, job, mood) {
+    super(name, job, mood);
+  }
+
+  delivery() {
+    console.log(`Heres your package ${this.name}`);
+  }
+  package() {
+    console.log(`Where did the package go?`);
+  }
+}
+
+class Chef extends Person {
+  constructor(name, job, mood) {
+    super(name, job, mood);
+  }
+
+  cook() {
+    console.log(`${this.name} made the seasonal menu!`);
+  }
+  saZon() {
+    console.log("This tastes bland");
+  }
+  status() {
+    console.log(`${this.mood}`);
+  }
+}
+
+const p1 = new PostalWorker("Colton", "Driver", "tired");
+
+const p2 = new PostalWorker("Abraham", "Runner", "i feel nothing");
+
+// console.log(p1);
+p1.delivery();
+p1.package();
+
+// console.log(p2);
+p2.delivery();
+p2.package();
+
+const c1 = new Chef("Remy", "Lead Chef", "hungry");
+const c2 = new Chef("Emile", "Taste Tester", "full");
+
+// console.log(c1);
+c1.cook();
+c1.saZon();
+
+// console.log(c2);
+c2.cook();
+c2.saZon();
+
