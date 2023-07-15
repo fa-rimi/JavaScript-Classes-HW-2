b1c06b0ee9846c090b026fdf810999a3ce661d1c
rimi <a.fatimaarimi@gmail.com>
2023-07-14 21:09:24 -0400

    fixed stuff

--- script.js
+++ script.js
@@ -41,20 +41,35 @@ class PostalWorker extends Person {
   constructor(name, job, mood) {
     super(name, job, mood);
   }
-
+  // action
   delivery() {
     console.log(`Heres your package ${this.name}`);
   }
   package() {
     console.log(`Where did the package go?`);
   }
+  moody() {
+    console.log(`I am ${this.mood}`);
+  }
 }
+
+const p1 = new PostalWorker("Colton", "Driver", "tired");
+const p2 = new PostalWorker("Abraham", "Runner", "i feel nothing");
+
+// console.log(p1);
+p1.delivery();
+p1.package();
+p1.moody();
+
+// console.log(p2);
+p2.delivery();
+p2.package();
+p2.moody();
 
 class Chef extends Person {
   constructor(name, job, mood) {
     super(name, job, mood);
   }
-
   cook() {
     console.log(`${this.name} made the seasonal menu!`);
   }
@@ -66,27 +81,17 @@ class Chef extends Person {
   }
 }
 
-const p1 = new PostalWorker("Colton", "Driver", "tired");
-
-const p2 = new PostalWorker("Abraham", "Runner", "i feel nothing");
-
-// console.log(p1);
-p1.delivery();
-p1.package();
-
-// console.log(p2);
-p2.delivery();
-p2.package();
-
 const c1 = new Chef("Remy", "Lead Chef", "hungry");
 const c2 = new Chef("Emile", "Taste Tester", "full");
 
 // console.log(c1);
 c1.cook();
 c1.saZon();
+c1.status();
 
 // console.log(c2);
 c2.cook();
 c2.saZon();
+c2.status()
 
 // Part 3 
